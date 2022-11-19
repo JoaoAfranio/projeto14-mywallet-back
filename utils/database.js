19c033d7627fe72eb4b879a9c43ef1f15a7f30e5
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-11-19 14:30:22 -0300

    adding port

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
 dotenv.config();
-
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
